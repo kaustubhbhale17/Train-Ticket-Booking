817cf783cc8d6847d95fffa8aaff00d6856677ea
kaustubhbhale17 <kaustubhbhaleytz@gmail.com>
2022-08-29 18:57:11 +0530

    initial commit

--- src/com/util/TicketApplication.java
+++ src/com/util/TicketApplication.java
@@ -1,4 +1,4 @@
-package com.tickets;
+package com.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
--- src/com/util/TrainDAO.java
+++ src/com/util/TrainDAO.java
@@ -1,4 +1,4 @@
-package com.tickets;
+package com.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
